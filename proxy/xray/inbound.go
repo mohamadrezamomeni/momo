5773d79c247d5da44f9cf60c70396ab7bf54b14b
mohamadreza <mohamadrezamomen97@gmail.com>
2025-04-13 23:17:18 +0330

    feat: discard streaming setting

--- proxy/xray/inbound.go
+++ proxy/xray/inbound.go
@@ -15,12 +15,9 @@ import (
 	statsService "github.com/xtls/xray-core/app/stats/command"
 	"github.com/xtls/xray-core/common/net"
 	"github.com/xtls/xray-core/common/protocol"
-	"github.com/xtls/xray-core/common/protocol/tls/cert"
 	"github.com/xtls/xray-core/common/serial"
 	"github.com/xtls/xray-core/core"
 	vmessInbound "github.com/xtls/xray-core/proxy/vmess/inbound"
-	"github.com/xtls/xray-core/transport/internet"
-	"github.com/xtls/xray-core/transport/internet/tls"
 )
 
 func (x *Xray) AddInbound(inpt *dto.AddInbound) (*serializer.AddInboundSerializer, error) {
@@ -40,16 +37,6 @@ func (x *Xray) AddInbound(inpt *dto.AddInbound) (*serializer.AddInboundSerialize
 				SniffingSettings: &proxyman.SniffingConfig{
 					Enabled:             true,
 					DestinationOverride: []string{"http", "tls"},
-				},
-				StreamSettings: &internet.StreamConfig{
-					ProtocolName: "websocket",
-
-					SecurityType: serial.GetMessageType(&tls.Config{}),
-					SecuritySettings: []*serial.TypedMessage{
-						serial.ToTypedMessage(&tls.Config{
-							Certificate: []*tls.Certificate{tls.ParseCertificate(cert.MustGenerate(nil))},
-						}),
-					},
 				},
 			}),
 
