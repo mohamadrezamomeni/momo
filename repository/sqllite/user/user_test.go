b0a3349d3a025ff199f6c56f82c495abf5332e52
mohamadreza <mohamadrezamomen97@gmail.com>
2025-04-18 00:21:31 +0330

    feat: test create

---
+++ repository/sqllite/user/user_test.go
@@ -0,0 +1,49 @@
+package user
+
+import (
+	"os"
+	"testing"
+
+	"momo/pkg/config"
+
+	"momo/repository/sqllite"
+	"momo/repository/sqllite/user/dto"
+)
+
+var (
+	email     string = "momo@world.com"
+	firstName string = "momo"
+	lastName  string = "momoian"
+	userRepo  IUserRepository
+)
+
+func TestMain(m *testing.M) {
+	cfg, err := config.Load("config.yaml")
+	if err != nil {
+		os.Exit(1)
+	}
+	db := sqllite.New(&cfg.DB)
+
+	userRepo = New(db)
+
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func TestCreate(t *testing.T) {
+	user, err := userRepo.Create(&dto.Create{
+		Email:     email,
+		FirstName: firstName,
+		LastName:  lastName,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if user.Email != email || user.FirstName != firstName || user.LastName != lastName || user.ID != "" {
+		t.Error("user creation requires some troubleshooting")
+		return
+	}
+}
