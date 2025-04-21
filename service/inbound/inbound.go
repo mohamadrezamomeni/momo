bcade6fd50908f38ddf6e0f460e3b92ed8c11af0
mohamadreza <mohamadrezamomen97@gmail.com>
2025-04-21 21:33:54 +0330

    feat: inboundservice

---
+++ service/inbound/inbound.go
@@ -0,0 +1,97 @@
+package service
+
+import (
+	"time"
+
+	"momo/entity"
+	"momo/proxy/vpn"
+	"momo/proxy/vpn/dto"
+	vpnSerializer "momo/proxy/vpn/serializer"
+)
+
+type Inbound struct {
+	inboundRepo inboundRepo
+	vpnProxy    vpnProxy
+	userService userService
+}
+
+type vpnProxy interface {
+	AddInbound(*dto.Inbound, string) (err error)
+	DisableInbound(inpt *dto.Inbound, VPNType string) error
+	GetTraffic(inpt *dto.Inbound, VPNType vpn.VPNType) (*vpnSerializer.Traffic, error)
+}
+
+type userService interface {
+	FindByID(string) (*entity.User, error)
+}
+
+type inboundRepo interface {
+	RetriveFaultyInbounds() ([]*entity.Inbound, error)
+}
+
+func New(repo inboundRepo, vpnService vpnProxy, userService userService) *Inbound {
+	return &Inbound{
+		inboundRepo: repo,
+		vpnProxy:    vpnService,
+		userService: userService,
+	}
+}
+
+func (i Inbound) ApplyChanges() error {
+	inbounds, err := i.inboundRepo.RetriveFaultyInbounds()
+	if err != nil {
+		return err
+	}
+
+	for _, inbound := range inbounds {
+		if i.mustItBeActive(inbound) {
+			i.activeInbound(inbound, inbound.VPNType)
+		} else {
+			i.deActiveInbound(inbound, inbound.VPNType)
+		}
+	}
+	return nil
+}
+
+func (i Inbound) mustItBeActive(inbound *entity.Inbound) bool {
+	if now := time.Now(); inbound.IsBlock == false &&
+		((now.Before(inbound.End) || now.Equal(inbound.End)) &&
+			(now.After(inbound.Start) || now.Equal(inbound.Start))) &&
+		inbound.IsActive {
+		return true
+	}
+	return false
+}
+
+func (i Inbound) deActiveInbound(inbound *entity.Inbound, vpnType vpn.VPNType) error {
+	info, err := i.getInfo(inbound)
+	if err != nil {
+		return err
+	}
+	return i.vpnProxy.DisableInbound(info, vpnType)
+}
+
+func (i Inbound) activeInbound(inbound *entity.Inbound, vpnType vpn.VPNType) error {
+	info, err := i.getInfo(inbound)
+	if err != nil {
+		return err
+	}
+	return i.vpnProxy.AddInbound(info, vpnType)
+}
+
+func (i Inbound) getInfo(inbound *entity.Inbound) (*dto.Inbound, error) {
+	user, err := i.userService.FindByID(inbound.UserID)
+	if err != nil {
+		return &dto.Inbound{}, err
+	}
+	info := &dto.Inbound{
+		User: &dto.User{
+			Username: user.Username,
+			ID:       user.ID,
+		},
+		Address: inbound.Domain,
+		Port:    inbound.Port,
+		Tag:     inbound.Tag,
+	}
+	return info, nil
+}
---
+++ service/vpn/vpn.go
@@ -0,0 +1,40 @@
+package vpn
+
+import (
+	"momo/entity"
+	vpnProxy "momo/proxy/vpn"
+	"momo/repository/sqllite/vpn/dto"
+)
+
+type vpnRepo interface {
+	Filter(*dto.FilterVPNs) ([]*entity.VPN, error)
+}
+
+type VPNService struct {
+	vpnRepo vpnRepo
+}
+
+func New(repo vpnRepo) *VPNService {
+	return &VPNService{
+		vpnRepo: repo,
+	}
+}
+
+func (v *VPNService) MakeProxy() (*vpnProxy.ProxyVPN, error) {
+	vpns, err := v.vpnRepo.Filter(&dto.FilterVPNs{})
+	if err != nil {
+		return nil, err
+	}
+
+	cfgs := make([]*vpnProxy.VPNConfig, 0)
+
+	for _, vpn := range vpns {
+		cfgs = append(cfgs, &vpnProxy.VPNConfig{
+			Port:    vpn.ApiPort,
+			Domain:  vpn.Domain,
+			VPNType: vpn.VPNType,
+		})
+	}
+
+	return vpnProxy.New(cfgs), nil
+}
