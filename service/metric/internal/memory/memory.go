28a32b16ce50c88cd39ff19f028b1621e97f72f8
mohamadreza <mohamadrezamomen97@gmail.com>
2025-04-27 11:06:34 +0330

    feat: get memory metric

---
+++ service/metric/internal/memory/memory.go
@@ -0,0 +1,65 @@
+package memory
+
+import (
+	"os"
+	"strconv"
+	"strings"
+
+	momoError "momo/pkg/error"
+)
+
+type MemoMetric struct {
+	memTotal     uint64
+	memFree      uint64
+	memAvailable uint64
+}
+
+func New() (*MemoMetric, error) {
+	dataRaw, err := getData()
+	if err != nil {
+		return nil, err
+	}
+
+	memTotal, memFree, memAvailable, err := extractData(dataRaw)
+
+	return &MemoMetric{
+		memTotal:     memTotal,
+		memFree:      memFree,
+		memAvailable: memAvailable,
+	}, nil
+}
+
+func extractData(dataRaw string) (uint64, uint64, uint64, error) {
+	var memTotal, memFree, memAvailable uint64
+	for _, line := range strings.Split(dataRaw, "\n") {
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			continue
+		}
+		key := fields[0]
+
+		value, err := strconv.ParseUint(fields[1], 10, 64)
+		if err != nil {
+			continue
+		}
+
+		switch key {
+		case "MemTotal:":
+			memTotal = value
+		case "MemFree:":
+			memFree = value
+		case "MemAvailable:":
+			memAvailable = value
+		}
+	}
+
+	return memTotal, memFree, memAvailable, nil
+}
+
+func getData() (string, error) {
+	data, err := os.ReadFile("/proc/meminfo")
+	if err != nil {
+		return "", momoError.DebuggingErrorf("error to open /proc/meminfo the problem was %v", err)
+	}
+	return string(data), nil
+}
