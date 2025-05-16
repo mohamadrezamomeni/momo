16725f7144f346de569deba4be64963c83cec351
mohamadreza <mohamadrezamomen97@gmail.com>
2025-05-16 23:09:49 +0330

    feat: handle users aren't approved

--- delivery/telegram/controller/middleware/identify_user.go
+++ delivery/telegram/controller/middleware/identify_user.go
@@ -1,8 +1,12 @@
 package middleware
 
 import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/mohamadrezamomeni/momo/delivery/telegram/core"
 	"github.com/mohamadrezamomeni/momo/entity"
+	momoError "github.com/mohamadrezamomeni/momo/pkg/error"
+	telegrammessages "github.com/mohamadrezamomeni/momo/pkg/telegram_messages"
+	"github.com/mohamadrezamomeni/momo/pkg/utils"
 )
 
 type UserService interface {
@@ -16,9 +20,22 @@ func IdentifyUser(userSvc UserService) core.Middleware {
 			if err != nil {
 				return nil, err
 			}
-			user, err := userSvc.FindByTelegramID(idStr)
+			id, err := utils.ConvertToInt64(idStr)
 			if err != nil {
 				return nil, err
+			}
+			user, err := userSvc.FindByTelegramID(idStr)
+			if momoErr, ok := momoError.GetMomoError(err); err != nil && (!ok || momoErr.GetErrorType() != momoError.NotFound) {
+				return nil, err
+			}
+
+			if user != nil && !user.IsApproved {
+				message, _ := telegrammessages.GetMessage("error.forbidden_access", map[string]string{})
+				return &core.ResponseHandlerFunc{
+					Result:       tgbotapi.NewMessage(id, message),
+					ReleaseState: true,
+					RedirectRoot: false,
+				}, nil
 			}
 			update.UserSystem = user
 			return next(update)
