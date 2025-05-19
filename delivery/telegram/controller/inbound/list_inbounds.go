00b5bd2192754a8fee28134803f767c3e46fef17
mohamadreza <mohamadrezamomen97@gmail.com>
2025-05-19 10:43:43 +0330

    refactor: refactor list inbounds

--- delivery/telegram/controller/inbound/list_inbounds.go
+++ delivery/telegram/controller/inbound/list_inbounds.go
@@ -7,6 +7,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/mohamadrezamomeni/momo/delivery/telegram/core"
 	inboundServiceDto "github.com/mohamadrezamomeni/momo/dto/service/inbound"
+	"github.com/mohamadrezamomeni/momo/entity"
 	telegrammessages "github.com/mohamadrezamomeni/momo/pkg/telegram_messages"
 	"github.com/mohamadrezamomeni/momo/pkg/utils"
 )
@@ -30,12 +31,7 @@ func (h *Handler) ListInbounds(update *core.Update) (*core.ResponseHandlerFunc, 
 	}
 
 	if len(inbounds) == 0 {
-		notFoundTitle, _ := telegrammessages.GetMessage("inbound.list.not_found", map[string]string{})
-		return &core.ResponseHandlerFunc{
-			Result:       tgbotapi.NewMessage(id, notFoundTitle),
-			ReleaseState: true,
-			RedirectRoot: true,
-		}, nil
+		return h.sendNotFoundInbounds(update.UserSystem)
 	}
 
 	var sb strings.Builder
@@ -45,59 +41,14 @@ func (h *Handler) ListInbounds(update *core.Update) (*core.ResponseHandlerFunc, 
 		return nil, err
 	}
 
-	active, err := telegrammessages.GetMessage("inbound.list.active", map[string]string{})
-	if err != nil {
-		return nil, err
-	}
-
-	deactive, err := telegrammessages.GetMessage("inbound.list.deactive", map[string]string{})
-	if err != nil {
-		return nil, err
-	}
-
-	block, err := telegrammessages.GetMessage("inbound.list.block", map[string]string{})
-	if err != nil {
-		return nil, err
-	}
-
-	unblock, err := telegrammessages.GetMessage("inbound.list.unblock", map[string]string{})
-	if err != nil {
-		return nil, err
-	}
-
 	sb.WriteString(title)
 
 	for i, inbound := range inbounds {
-		idReport, err := telegrammessages.GetMessage("inbound.list.id_report", map[string]string{
-			"counter": strconv.Itoa(i + 1),
-			"id":      strconv.Itoa(inbound.ID),
-		})
+		item, err := h.writeItem(i, inbound)
 		if err != nil {
 			return nil, err
 		}
-		trafficReport, err := telegrammessages.GetMessage("inbound.list.inbound_traffic_used", map[string]string{
-			"traffic_used":  strconv.Itoa(int(inbound.TrafficUsage)),
-			"traffic_limit": strconv.Itoa(int(inbound.TrafficLimit)),
-		})
-		if err != nil {
-			return nil, err
-		}
-
-		sb.WriteString(idReport)
-		sb.WriteString(trafficReport)
-		if inbound.IsActive {
-			sb.WriteString(active)
-		} else {
-			sb.WriteString(deactive)
-		}
-		sb.WriteString(" | ")
-		if inbound.IsBlock {
-			sb.WriteString(block)
-		} else {
-			sb.WriteString(unblock)
-		}
-		sb.WriteString("\n\n")
-
+		sb.WriteString(item)
 	}
 	message := sb.String()
 	msg := tgbotapi.NewMessage(id, message)
@@ -107,4 +58,65 @@ func (h *Handler) ListInbounds(update *core.Update) (*core.ResponseHandlerFunc, 
 		ReleaseState: true,
 		RedirectRoot: true,
 	}, nil
+}
+
+func (h *Handler) getStatus() (string, string, string, string, error) {
+	active, err := telegrammessages.GetMessage("inbound.list.active", map[string]string{})
+	if err != nil {
+		return "", "", "", "", err
+	}
+
+	deactive, err := telegrammessages.GetMessage("inbound.list.deactive", map[string]string{})
+	if err != nil {
+		return "", "", "", "", err
+	}
+
+	block, err := telegrammessages.GetMessage("inbound.list.block", map[string]string{})
+	if err != nil {
+		return "", "", "", "", err
+	}
+
+	unblock, err := telegrammessages.GetMessage("inbound.list.unblock", map[string]string{})
+	if err != nil {
+		return "", "", "", "", err
+	}
+
+	return active, deactive, block, unblock, nil
+}
+
+func (h *Handler) writeItem(i int, inbound *entity.Inbound) (string, error) {
+	var sb strings.Builder
+
+	active, deactive, block, unblock, err := h.getStatus()
+
+	idReport, err := telegrammessages.GetMessage("inbound.list.id_report", map[string]string{
+		"counter": strconv.Itoa(i + 1),
+		"id":      strconv.Itoa(inbound.ID),
+	})
+	if err != nil {
+		return "", err
+	}
+	trafficReport, err := telegrammessages.GetMessage("inbound.list.inbound_traffic_used", map[string]string{
+		"traffic_used":  strconv.Itoa(int(inbound.TrafficUsage)),
+		"traffic_limit": strconv.Itoa(int(inbound.TrafficLimit)),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	sb.WriteString(idReport)
+	sb.WriteString(trafficReport)
+	if inbound.IsActive {
+		sb.WriteString(active)
+	} else {
+		sb.WriteString(deactive)
+	}
+	sb.WriteString(" | ")
+	if inbound.IsBlock {
+		sb.WriteString(block)
+	} else {
+		sb.WriteString(unblock)
+	}
+	sb.WriteString("\n\n")
+	return sb.String(), nil
 }
