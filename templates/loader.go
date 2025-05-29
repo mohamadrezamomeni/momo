4150adb6ed8c61219a2de254af7e68afcdb77f03
mohamadreza <mohamadrezamomen97@gmail.com>
2025-05-29 16:16:02 +0330

    fix: address from root in load templates

--- templates/loader.go
+++ templates/loader.go
@@ -3,14 +3,36 @@ package templates
 import (
 	"fmt"
 	"os"
+	"path"
 	"strings"
+
+	momoError "github.com/mohamadrezamomeni/momo/pkg/error"
+	"github.com/mohamadrezamomeni/momo/pkg/utils"
 )
 
-func LoadClientConfig(domain string, port string, userID string) (string, error) {
-	templateByte, err := os.ReadFile("client_config.tpl")
+func getFilename(name string) (string, error) {
+	scope := "templates.getFilename"
+	root, err := utils.GetRootOfProject()
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		return "", momoError.Wrap(err).Scope(scope).Input(name).ErrorWrite()
+	}
+	filename := fmt.Sprintf("%s.tpl", name)
+
+	path := path.Join(root, "templates", filename)
+	return path, nil
+}
+
+func LoadClientConfig(domain string, port string, userID string) (string, error) {
+	scope := "template.LoadClientConfig"
+
+	path, err := getFilename("client_config")
+	if err != nil {
 		return "", err
+	}
+
+	templateByte, err := os.ReadFile(path)
+	if err != nil {
+		return "", momoError.Wrap(err).Scope(scope).Input(domain, port, userID).ErrorWrite()
 	}
 	template := string(templateByte)
 
