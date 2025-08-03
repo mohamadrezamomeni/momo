31bb500300edaf68c4a2776d8dfa9af923f21245
mohamadreza <mohamadrezamomen97@gmail.com>
2025-08-03 15:30:24 +0330

    fix: check empty user

--- delivery/telegram/controller/middleware/validate_access.go
+++ delivery/telegram/controller/middleware/validate_access.go
@@ -3,14 +3,19 @@ package middleware
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/mohamadrezamomeni/momo/delivery/telegram/core"
+	momoError "github.com/mohamadrezamomeni/momo/pkg/error"
 	telegrammessages "github.com/mohamadrezamomeni/momo/pkg/telegram_messages"
 	"github.com/mohamadrezamomeni/momo/pkg/utils"
 )
 
 func ValidateAccess() core.Middleware {
+	scope := "telegram.middleware.ValidateAccess"
 	return func(next core.HandlerFunc) core.HandlerFunc {
 		return func(update *core.Update) (*core.ResponseHandlerFunc, error) {
 			user := update.UserSystem
+			if user == nil {
+				return nil, momoError.Scope(scope).Errorf("the user is nill")
+			}
 			id, err := utils.ConvertToInt64(user.TelegramID)
 			if err != nil {
 				return nil, err
