ef268396100c6f5c4a17b65d5f4f5c18ed1ab629
mohamadreza <mohamadrezamomen97@gmail.com>
2025-08-18 20:30:34 +0330

    fix: validate finding create_user_tier

--- validator/user_tier/create_user_tier.go
+++ validator/user_tier/create_user_tier.go
@@ -1,26 +1,28 @@
 package user
 
 import (
+	"fmt"
+
 	userTierControllerDto "github.com/mohamadrezamomeni/momo/dto/controller/user_tier"
 	momoError "github.com/mohamadrezamomeni/momo/pkg/error"
 )
 
 func (v *Validator) ValidateCreatingUserTier(data userTierControllerDto.Create) error {
 	scope := "validator.user.ValidateCreatingUserTier"
-
 	user, err := v.userSvc.FindByID(data.UserID)
+	fmt.Println(user, err)
 	if err != nil {
 		return momoError.Wrap(err).Scope(scope).BadRequest().Input(data).ErrorWrite()
 	}
-	if user != nil {
+	if user == nil {
 		return momoError.Scope(scope).BadRequest().Input(data).ErrorWrite()
 	}
-
 	tier, err := v.tierSvc.FindByName(data.Tier)
+	fmt.Println(tier, err)
 	if err != nil {
 		return momoError.Wrap(err).Scope(scope).BadRequest().Input(data).ErrorWrite()
 	}
-	if tier != nil {
+	if tier == nil {
 		return momoError.Scope(scope).BadRequest().Input(data).ErrorWrite()
 	}
 	return nil
